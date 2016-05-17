1a9d5d71d8f050c4b475f29f5daafcc802078074
jpomeda <jpomeda@die.upm.es>
2016-05-17 19:27:08 +0200

    añadida la activación de Bluetooth a la app.

--- app/src/main/java/com/upm/etsit/b105/jpomeda/bulletinledboard/UserActivity.java
+++ app/src/main/java/com/upm/etsit/b105/jpomeda/bulletinledboard/UserActivity.java
@@ -3,10 +3,13 @@ package com.upm.etsit.b105.jpomeda.bulletinledboard;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.bluetooth.*;
+import android.content.Intent;
 
 public class UserActivity extends AppCompatActivity {
 
     private TextView usuario;
+    private final static int REQUEST_ENABLE_BT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +20,22 @@ public class UserActivity extends AppCompatActivity {
 
         Bundle bundle = this.getIntent().getExtras();
         usuario.setText("Bienvenido " + bundle.getString("Usuario"));
+
+
+        /* Creamos un BluetoothAdapter, cogiendo el de por defecto del teléfono (normalmente
+        sólo tenemos ese).
+         */
+
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            throw new NullPointerException(){
+
+            };
+        }
+
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
     }
 }
